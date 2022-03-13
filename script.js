2e85646f9d1882ac8d5cf9e01563c379946a6dc8
Beverlyfiona12 <awuorrae@gmail.com>
2022-03-13 22:09:07 +0300

    js function

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ $(document).ready(function(){
       $("#development").click(function() {
         $(".development, #development").toggle("slow");
       });
-    
+      $(".product").click(function() {
+        $("#product, .product").toggle("slow");
+      });
+      $("#product").click(function() {
+        $(".product, #product").toggle("slow");
+      });
   });
   
   
