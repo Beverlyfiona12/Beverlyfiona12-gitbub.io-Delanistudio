0e7418d74fb91efc939be9b45bfe6c97e2ce95f5
Beverlyfiona12 <awuorrae@gmail.com>
2022-03-13 22:04:59 +0300

    Development toggling function

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
     $(".design").click(function() {
       $("#design, .design").toggle("slow");
     });
-   
+    $("#design").click(function() {
+        $(".design, #design").toggle("slow");
+      });
+      $(".development").click(function() {
+        $("#development, .development").toggle("slow");
+      });
+      $("#development").click(function() {
+        $(".development, #development").toggle("slow");
+      });
+    
   });
   
   
